7e97fe01847de6049f6be15a769426609bd55e89
avivashishta29 <avivashishta@gmail.com>
2023-02-07 21:02:47 +0530

    header + footer

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "../components/Common/Footer";
+import Header from "../components/Common/Header";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default HomePage;
