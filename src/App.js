932bc40520dafb4d8be900eeeb882c50dc38c65d
avivashishta29 <avivashishta@gmail.com>
2023-02-07 20:31:36 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import DashboardPage from "./pages/dashboard";
+import HomePage from "./pages/home";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          {/* <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/compare" element={<ComparePage />} />
+          <Route path="/watchlist" element={<WatchlistPage />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
